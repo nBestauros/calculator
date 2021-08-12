626af5554d23978f1cf95c0ebe9734b033834125
Nic <nbestauros@gmail.com>
2021-08-11 17:12:53 -0700

    Added basic calculator UI.

---
+++ scripts.js
@@ -0,0 +1,14 @@
+function operate(num1, num2, operation){
+    if(operation=="add"){
+        return num1+num2;
+    }
+    if(operation=="subtract"){
+        return num1-num2;
+    }
+    if(operation=="multiply"){
+        return num1*num2;
+    }
+    if(operation=="divide"){
+        return num1/num2;
+    }
+}
